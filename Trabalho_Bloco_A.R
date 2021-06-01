4431b254042763e4192dd9d2468018d7d6de9163
FelipeDelamar <66812468+FelipeDelamar@users.noreply.github.com>
2021-06-01 00:38:34 -0300

    Update Trabalho_Bloco_A.R

--- Trabalho_Bloco_A.R
+++ Trabalho_Bloco_A.R
@@ -142,7 +142,33 @@ View(df_dolar_brent3)
 #Alteração Felipe -------------------------------------
 df_imp_dolar_brent<-merge(df_dolar_brent3,df_impostos3,by="mes")
 
+#Primeiramente vou gerar um novo dataset filtrando apenas o Brasil para analise (sem levar em consideração por região)
+df_imp_dolar_brent_brasil <- subset(df_imp_dolar_brent, Sigla_regiao== 'BR')
 
+#Após feito a junção dos dados em um único dataset, será plotado algumas visões
+
+
+#Gráfico da variação do dolar e do barril de petroleo (brent)
+linhaDolar <- ggplot(data=df_imp_dolar_brent_brasil, aes(x=mes)) +
+  geom_line(aes(y = CotacaoDolar, group=1), color = "darkred", size=2)+
+  ggtitle("Cotação dólar 2018-07 até 2020-07") +
+  ylab("Cotação")
+
+linhaBrent <- ggplot(data=df_imp_dolar_brent_brasil, aes(x=mes)) +
+  geom_line(aes(y = CotacaoBrentReais, group=2), color = "steelblue", size=2)+
+  ggtitle("Cotação Barril em Real 2018-07 até 2020-07") +
+  ylab("Cotação")  
+  
+linhaDolar+linhaBrent
+
+
+#gráfico da variação do preço da gasolina pelo produtor
+linhaVarProdutor <- ggplot(data=df_imp_dolar_brent_brasil, aes(x=mes)) +
+  geom_line(aes(y = mediaPrecoGasolina, group=1), color = "darkred", size=2)+
+  ggtitle("Preço produtor gasolina 2018-07 até 2020-07") +
+  ylab("Cotação")
+
+linhaDolar+linhaBrent+linhaVarProdutor
 
 #Fim alteração Felipe ------------------
 
